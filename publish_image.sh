4952a4a5961137b49623c3d7221bdc763086ed96
Qin Zhao <q.zhao@f5.com>
2019-08-26 18:57:18 +0800

    Fix docker image tag

--- publish_image.sh
+++ publish_image.sh
@@ -3,7 +3,7 @@
 TAG=$(git describe --tags --abbrev=0)
 docker login -u zhaoqin -p $PASSWORD
 echo "Building zhaoqin/fluentd:$TAG"
-docker build -t zhaoqin/fluentd:latest .
+docker build -t zhaoqin/fluentd:$TAG .
 echo "Publishing zhaoqin/fluentd:$TAG"
-docker push zhaoqin/fluentd:latest
+docker push zhaoqin/fluentd:$TAG
 docker logout
