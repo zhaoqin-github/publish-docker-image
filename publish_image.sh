79c0962e04e95f255fd6290e27d7a2782f850e60
Qin Zhao <q.zhao@f5.com>
2019-08-26 18:52:59 +0800

    Publish image with tag

--- publish_image.sh
+++ publish_image.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 
-echo "hello"
+TAG=$(git describe --tags --abbrev=0)
 docker login -u zhaoqin -p $PASSWORD
+echo "Building zhaoqin/fluentd:$TAG"
 docker build -t zhaoqin/fluentd:latest .
+echo "Publishing zhaoqin/fluentd:$TAG"
 docker push zhaoqin/fluentd:latest
 docker logout
